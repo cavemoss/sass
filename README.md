f7d521d7e41080e1c1d5e997b51164e55e1aa5ca
mossy boy <alexshu1812@gmail.com>
2024-04-30 15:13:49 +0800

    two

---
+++ README.md
@@ -0,0 +1,7 @@
+## Gulp Runtime SASS Compiler 
+
+`npm install` Install dependencies
+
+`npm run dev` Run compiler
+
+> All .scss files in `src/` will be getting converted to .css and output in `css/`
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('./index.scss').pipe(sass()).pipe(dest('css'))
+    return src('./src/*.scss').pipe(sass()).pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['./index.scss'], {ignoreInitial: false}, buildStyles)
+    watch(['./src/*.scss'], {ignoreInitial: false}, buildStyles)
 }
 
 exports.default = series(watchTask) 
