c50b10b30954f350d2c970c153dfbb979ca2b956
mossy boy <alexshu1812@gmail.com>
2024-05-25 14:55:48 +0800

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+const { src, dest, watch, series } = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+
+let ext = "scss" // scss sass
 
 function buildStyles() {
-    return src('./src/*.scss').pipe(sass()).pipe(dest('css'))
+    return src(`./src/*.${ext}`).pipe(sass()).pipe(dest("css"))
 }
 
-function watchTask() {
-    watch(['./src/*.scss'], {ignoreInitial: false}, buildStyles)
+async function watchTask() {
+    watch([`./src/*.${ext}`], {ignoreInitial: false}, buildStyles)
 }
 
 exports.default = series(watchTask) 
 
-// run `npx gulp` to run sass compiler
+// run `npm run dev` to run sass compiler
